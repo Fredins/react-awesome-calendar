05be1426c26765da54bcec41adb9b558b34e20f8
fredins <fredin.martin@gmail.com>
2022-05-22 12:38:40 +0200

    Modify example

--- example/src/App.js
+++ example/src/App.js
@@ -4,9 +4,9 @@ import Calendar from 'react-awesome-calendar';
 
 const events = [{
   id: 1,
-  color: '#fd3153',
-  from: '2022-04-03T18:00:00+00:00',
-  to: '2022-04-03T19:00:00+00:00',
+  color: '#B9ACF2',
+  from: '2020-04-03T18:00:00+00:00',
+  to: '2020-04-03T19:00:00+00:00',
   title: 'Seeing family',
 }];
 
